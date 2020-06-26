7e977349e4bffca2879bf808c780fd08d69c8e51
Viviane Ramos <ramosluzviviane@gmail.com>
2020-06-26 14:55:49 -0300

    2 commit

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ function covidState(e){
             div.innerHTML += `<p>Mortes: ☠️${dados.deaths}</p>`
             
         })
+        .catch( error => div.innerHTML = `<p>${error}! Não foi possível buscar os dados.</p>`)
         
 }
 
@@ -64,6 +65,7 @@ function covidCountry(e){
             div.innerHTML += `<p>Mortes:☠️ ${dados.data.deaths}</p>`
             
         })
+        .catch( error => div.innerHTML = `<p>${error}! Não foi possível buscar os dados.</p>`)
 }
 
 ufSelect.addEventListener('change', covidState)
